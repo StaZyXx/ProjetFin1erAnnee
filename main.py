489198065ae6bf1c1d3805912bc4e04df449cfe6
StaZyXx <samuel.letellier@supinfo.Com>
2023-01-20 14:14:11 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Case:
+    def __init__(self):
+        self.__is_empty = True
+        self.__player = 0
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    EAST = 3
+    WEST = 4
+    
+#class Barrier:
+
