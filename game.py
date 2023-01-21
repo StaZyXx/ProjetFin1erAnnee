53170f8c0d0516b0863b8f3a3bcd7424ea39a3a0
ben.tintin.bp@gmail.com <ben.tintin.bp@gmail.com>
2023-01-21 16:38:39 +0100

    change type !

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from case import Case
 from player import Player
 
@@ -5,10 +7,10 @@ from player import Player
 class Game:
 
     def __init__(self):
-        self.__player: list[Player] = []
+        self.__player: List[Player] = []
         self.__board_size = 0
 
-        self.__cases: list[list[Case]] = []
+        self.__cases: List[List[Case]] = []
 
     def create_board(self):
         self.__cases = [[Case() for _ in range(self.__board_size)] for _ in range(self.__board_size)]
