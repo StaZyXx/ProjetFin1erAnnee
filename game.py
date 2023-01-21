674c6d651277da6288b0808d6004de54599aa55c
Goldlulu <luc.mialhe@hotmail.com>
2023-01-21 17:03:33 +0100

    Modification de la fonction create_board

--- game.py
+++ game.py
@@ -11,6 +11,31 @@ class Game:
         self.__board_size = 0
 
         self.__cases: List[List[Case]] = []
-
+        
     def create_board(self):
-        self.__cases = [[Case() for _ in range(self.__board_size)] for _ in range(self.__board_size)]
+        x=self.__board_size*2-1
+        y,z=2,2
+        board=[]
+
+        for _ in range (x):
+            add = []
+            if z%2==0:
+                for _ in range (x):
+                    if y%2==0:
+                        Case(0)
+                        y-=1
+                    else:
+                        Case(1)
+                        y+=1
+                z-=1
+            else:
+                for _ in range(x):
+                    if y%2==0:
+                        Case(2)
+                        y-=1
+                    else:
+                        Case(1)
+                        y+=1
+                z-=1
+            board.append(add)
+        return board
