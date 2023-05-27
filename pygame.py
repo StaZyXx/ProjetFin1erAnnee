3940f75e46727d3b4d490fbaf46628a9d639b13a
StaZyXx <samuel.letellier@supinfo.Com>
2023-05-27 14:01:13 +0200

    Start work on pygame

---
+++ pygame.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame import *
+
+
+class View:
+    def __init__(self):
+        pygame.init()
+        self.__running = True
+        pygame.display.set_caption("Quorridor")  # Nom de la fenêtre
+        self.__screen = pygame.display.set_mode((1500, 850), RESIZABLE)  # Définit la taille de la fenetre
+
+        self.__background = pygame.image.load("C:/Users/quent/Downloads/bg.jpg").convert()  # Charge l'image
+        self.__quorridor = pygame.image.load("C:/Users/quent/Downloads/QUORRIDOR.png").convert_alpha()
+
+        self.__screen.blit(self.__background, (0, 0))  # la positionne
+        self.__screen.blit(self.__quorridor, (400, 75))
+        pygame.display.flip()
+
+        while self.__running:
+            for event in pygame.event.get():  # récupérer un event
+                if event.type == pygame.QUIT:  # Si l'event est du type fermer la fenetre
+                    self.__running = False
+                    pygame.quit()
+
+
+View()
