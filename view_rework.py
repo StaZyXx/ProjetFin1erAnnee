b00483adb70d0547f36664619eb0b6f0d7db528e
MATHIS-PC-FIXE\Mathis <lecomtemathis31@gmail.com>
2023-06-11 17:51:38 +0200

    continue pygame

--- view_rework.py
+++ view_rework.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 
+import time
+
 
 class View:
     def __init__(self):
@@ -8,11 +10,14 @@ class View:
         self.__WHITE = (255, 255, 255, 50)
         self.__BLACK = (0, 0, 0)
         self.__BLUE = (171, 242, 255, 50)
+        self.__DARK_BLUE = (57, 73, 116, 200)
 
         self.home_page()
 
+        self.__button1 = 1
         # Boucle du jeu
         while self.__running:
+            print(self.__button1)
             for event in pygame.event.get():  # récupérer un event
                 if event.type == pygame.QUIT:  # Si l'event est du type fermer la fenetre
                     self.__running = False
@@ -20,11 +25,33 @@ class View:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     self.__cursor_pos = pygame.mouse.get_pos()
                     if self.__get_solo.collidepoint(self.__cursor_pos):
-                        print("solo")
+                        if self.__button1 == 1:
+                            print("solo")
+                        self.__running = False
                     elif self.__get_multiplayer.collidepoint(self.__cursor_pos):
+                        if self.__button1 == 1:
+                            print("multiplayer")
+                    elif self.__get_leave.collidepoint(self.__cursor_pos):
+                        if self.__button1 == 1:
+                            print("leave")
+
+        self.__button1 = 0
+        print(self.__button1)
+
+        self.page2()
+        self.__running = True
+        while self.__running:
+            for event in pygame.event.get():  # récupérer un event
+                if event.type == pygame.QUIT:  # Si l'event est du type fermer la fenetre
+                    self.__running = False
+                    pygame.quit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.__cursor_pos = pygame.mouse.get_pos()
+                    if self.__get_multiplayer.collidepoint(self.__cursor_pos):
                         print("multiplayer")
                     elif self.__get_leave.collidepoint(self.__cursor_pos):
                         print("leave")
+
 
     def home_page(self):
         pygame.init()
@@ -40,7 +67,7 @@ class View:
         pygame.draw.rect(self.__blue_image, self.__WHITE, (525, 100, 520, 100))
         self.__quorridor = self.__font.render('QUORRIDOR', False, (self.__BLACK))
 
-        pygame.draw.rect(self.__blue_image, self.__BLUE, (460, 300, 650, 75))
+        self.__rect_solo = pygame.draw.rect(self.__blue_image, self.__BLUE, (460, 300, 650, 75))
         self.__solo = self.__font.render('Solo', False, (self.__WHITE))
         self.__get_solo = self.__solo.get_rect()
         self.__get_solo.topleft = (700, 300)
@@ -62,6 +89,29 @@ class View:
         self.__screen.blit(self.__leave, (700, 610))
 
         pygame.display.flip()  # Mettre a jour l'affichage
+
+    def page2(self):
+        pygame.init()
+
+        self.__size = (1500, 850)
+        self.__blue_image = pygame.Surface(self.__size, pygame.SRCALPHA)
+
+
+        pygame.draw.rect(self.__blue_image, self.__BLUE, (100, 0, 1000, 500))
+
+        pygame.draw.rect(self.__blue_image, self.__DARK_BLUE, (100, 100, 200, 75))
+
+
+
+        self.__screen.blit(self.__background, (0, 0))
+        self.__screen.blit(self.__blue_image, (0, 0))
+
+
+
+        pygame.display.flip()  # Mettre a jour l'affichage
+
+
+
 
 
 View()
