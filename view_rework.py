5e2b045abb47073d0fb9a1664905956402f102f4
le-grand-ours <lecomtemathis31@gmail.com>
2023-06-13 18:28:11 +0200

    advevencement join

--- view_rework.py
+++ view_rework.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.locals import *
 
 class View:
     def __init__(self):
@@ -169,19 +169,19 @@ class View:
 
         self.__size_5x5 = self.__48_font.render('5x5', False, (self.__WHITE))
         self.__get_size_5x5 = self.__size_5x5.get_rect()
-        self.__get_size_5x5.topleft = (365, 390)
+        self.__get_size_5x5.topleft = (400, 400)
 
         self.__size_7x7 = self.__48_font.render('7x7', False, (self.__WHITE))
         self.__get_size_7x7 = self.__size_7x7.get_rect()
-        self.__get_size_7x7.topleft = (565, 390)
+        self.__get_size_7x7.topleft = (600, 400)
 
         self.__size_9x9 = self.__48_font.render('9x9', False, (self.__WHITE))
         self.__get_size_9x9 = self.__size_9x9.get_rect()
-        self.__get_size_9x9.topleft = (820, 390)
+        self.__get_size_9x9.topleft = (850, 400)
 
         self.__size_11x11 = self.__48_font.render('11x11', False, (self.__WHITE))
         self.__get_size_11x11 = self.__size_11x11.get_rect()
-        self.__get_size_11x11.topleft = (1020, 390)
+        self.__get_size_11x11.topleft = (1050, 400)
 
         # choix nombre barrière
         pygame.draw.rect(self.__blue_image2, self.__DARK_BLUE, (700, 500, 200, 50))
@@ -244,6 +244,9 @@ class View:
                         self.boucle_param("multiplayer")
                     elif self.__get_back.collidepoint(self.__cursor_pos):
                         self.boucle_home_page()
+                    elif self.__get_client.collidepoint(self.__cursor_pos):
+                        print("je suis con")
+                        self.boucle_join_page()
 
     def choice_server_client(self):
         pygame.init()
@@ -273,6 +276,153 @@ class View:
 
 
         pygame.display.flip()
+
+    def boucle_join_page(self):
+        self.__adress = ""
+        self.join_page()
+        self.__running = True
+        while self.__running:
+            for event in pygame.event.get():  # récupérer un event
+                if event.type == pygame.QUIT:  # Si l'event est du type fermer la fenetre
+                    self.__running = False
+                    pygame.quit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_1 or event.key == K_KP1:
+                        self.__adress = self.__adress + "1"
+                    elif event.key == K_2 or event.key == K_KP2:
+                        self.__adress = self.__adress + "2"
+                    elif event.key == K_3 or event.key == K_KP3:
+                        self.__adress = self.__adress + "3"
+                    elif event.key == K_4 or event.key == K_KP4:
+                        self.__adress = self.__adress + "4"
+                    elif event.key == K_5 or event.key == K_KP5:
+                        self.__adress = self.__adress + "5"
+                    elif event.key == K_6 or event.key == K_KP6:
+                        self.__adress = self.__adress + "6"
+                    elif event.key == K_7 or event.key == K_KP7:
+                        self.__adress = self.__adress + "7"
+                    elif event.key == K_8 or event.key == K_KP8:
+                        self.__adress = self.__adress + "8"
+                    elif event.key == K_9 or event.key == K_KP9:
+                        self.__adress = self.__adress + "9"
+                    elif event.key == K_0 or event.key == K_KP0:
+                        self.__adress = self.__adress + "0"
+                    elif event.key == K_KP_PERIOD or event.key == K_PERIOD:
+                        self.__adress = self.__adress + "."
+                    elif event.key == K_DELETE :
+                        self.__adress = self.__adress[:1]
+                        print(self.__adress)
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.__cursor_pos = pygame.mouse.get_pos()
+                    if self.__get_1.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "1"
+                    elif self.__get_2.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "2"
+                    elif self.__get_3.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "3"
+                    elif self.__get_4.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "4"
+                    elif self.__get_5.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "5"
+                    elif self.__get_6.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "6"
+                    elif self.__get_7.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "7"
+                    elif self.__get_8.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "8"
+                    elif self.__get_9.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "9"
+                    elif self.__get_0.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "0"
+                    elif self.__get_point.collidepoint(self.__cursor_pos):
+                        self.__adress = self.__adress + "."
+
+                self.join_page()
+
+
+    def join_page(self):
+        pygame.init()
+        self.__blue_image4 = pygame.Surface((1500, 850), pygame.SRCALPHA)
+
+        #carré
+        pygame.draw.rect(self.__blue_image4, self.__BLUE, (50, 50, 1400, 750))
+
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (100, 200, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (100, 300, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (100, 400, 50, 50))
+
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (400, 200, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (400, 300, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (400, 400, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (400, 500, 50, 50))
+
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (700, 200, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (700, 300, 50, 50))
+        pygame.draw.rect(self.__blue_image4, self.__DARK_BLUE, (700, 400, 50, 50))
+
+        #lettre
+        self.__1 = self.__96_font.render("1", False, (self.__WHITE))
+        self.__get_1 = self.__1.get_rect()
+        self.__get_1.topleft = (100, 200)
+        self.__4 = self.__96_font.render("4", False, (self.__WHITE))
+        self.__get_4 = self.__4.get_rect()
+        self.__get_4.topleft = (100, 300)
+        self.__7 = self.__96_font.render("7", False, (self.__WHITE))
+        self.__get_7 = self.__7.get_rect()
+        self.__get_7.topleft = (100, 400)
+
+        self.__2 = self.__96_font.render("2", False, (self.__WHITE))
+        self.__get_2 = self.__2.get_rect()
+        self.__get_2.topleft = (400, 200)
+        self.__5 = self.__96_font.render("5", False, (self.__WHITE))
+        self.__get_5 = self.__5.get_rect()
+        self.__get_5.topleft = (400, 300)
+        self.__8 = self.__96_font.render("8", False, (self.__WHITE))
+        self.__get_8 = self.__8.get_rect()
+        self.__get_8.topleft = (400, 400)
+        self.__0 = self.__96_font.render("0", False, (self.__WHITE))
+        self.__get_0 = self.__0.get_rect()
+        self.__get_0.topleft = (400, 500)
+
+        self.__3 = self.__96_font.render("3", False, (self.__WHITE))
+        self.__get_3 = self.__3.get_rect()
+        self.__get_3.topleft = (700, 200)
+        self.__6 = self.__96_font.render("6", False, (self.__WHITE))
+        self.__get_6 = self.__6.get_rect()
+        self.__get_6.topleft = (700, 300)
+        self.__9 = self.__96_font.render("9", False, (self.__WHITE))
+        self.__get_9 = self.__9.get_rect()
+        self.__get_9.topleft = (700, 400)
+        self.__point = self.__96_font.render(".", False, (self.__WHITE))
+        self.__get_point = self.__point.get_rect()
+        self.__get_point.topleft = (700, 500)
+
+        self.__adress_final1 = self.__96_font.render("Adresse de connexion :", False, (self.__WHITE))
+        self.__adress_final2 = self.__96_font.render(self.__adress, False, (self.__WHITE))
+
+        self.__screen.blit(self.__background, (0, 0))
+        self.__screen.blit(self.__blue_image4, (0, 0))
+
+        self.__screen.blit(self.__1, (100, 200))
+        self.__screen.blit(self.__4, (100, 300))
+        self.__screen.blit(self.__7, (100, 400))
+
+        self.__screen.blit(self.__2, (400, 200))
+        self.__screen.blit(self.__5, (400, 300))
+        self.__screen.blit(self.__8, (400, 400))
+        self.__screen.blit(self.__0, (400, 500))
+
+        self.__screen.blit(self.__3, (700, 200))
+        self.__screen.blit(self.__6, (700, 300))
+        self.__screen.blit(self.__9, (700, 400))
+        self.__screen.blit(self.__point, (700, 500))
+
+        self.__screen.blit(self.__adress_final1, (100, 700))
+        self.__screen.blit(self.__adress_final2, (875, 700))
+
+        pygame.display.flip()
+
 
 
 View()
